4bd4cfccd8f6fbda0e25dae4ba5fe4712de2220f
MichaelRomero <liikemike@aim.com>
2019-10-10 14:03:20 -0400

    fixed small style-conflict issue where input boxes were overriden from feedStyle

--- client/containers/WelcomeContainer.jsx
+++ client/containers/WelcomeContainer.jsx
@@ -2,6 +2,10 @@ import React, { Component, Fragment } from 'react';
 import NavigationBar from '../components/NavigationBar.jsx'
 import FeedContainer from './FeedContainer.jsx'
 import SidebarContainer from './SidebarContainer.jsx'
+// Styles
+import feedStyles from '../assets/styles/feed.css'
+import feedStylesMed from '../assets/styles/feed-med.css'
+import feedStylesLg from '../assets/styles/feed-lg.css'
 
 class WelcomeContainer extends Component {
   render() {
--- client/index.js
+++ client/index.js
@@ -3,19 +3,16 @@
 */
 import React from 'react';
 import { render } from 'react-dom';
-// import App from './components/App.jsx'
+import App from './components/App.jsx'
 import indexStyles from './assets/styles/index.css';
 import indexMedStyles from './assets/styles/index-med.css'
 // ! Used for testing, remove later
 import WelcomeContainer from './containers/WelcomeContainer.jsx'
-import feedStyles from './assets/styles/feed.css'
-import feedStylesMed from './assets/styles/feed-med.css'
-import feedStylesLg from './assets/styles/feed-lg.css'
 
 
 
 render(
-  <WelcomeContainer />, 
+  <App />, 
   document.getElementById('root')
   )
 // render(
